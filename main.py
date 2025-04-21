2996344007a6d55c2f4c79ce8a38e15fa99cf8fc
Giserrano <Giserrano@s.sfusd.edu>
2025-04-21 16:30:20 +0000

    4/21/25

--- main.py
+++ main.py
@@ -6,23 +6,36 @@ print("""
       Hello player, welcome to the game of Loteria!
       """)
 
-#translate
+#translate and track score
 def main(loteria_dict):
+    correct_answer = 0
+    wrong_answer = 0
+    need_work = []
     for key in loteria_dict:
         translation = input(f"What does {key} mean?: ")
         capitalized_translation = translation.title()
         print(capitalized_translation)
         if capitalized_translation == loteria_dict[key]:
             print("Correct")
+            correct_answer += 1
+            print(f"{correct_answer} correct.")
         else: 
             print(f"Incorrect, it means {loteria_dict[key]}")
+            wrong_answer += 1
+            print(f"{wrong_answer} wrong.")
+            need_work.append(key)
+        spaced_repitition(need_work)
 
-    
+def spaced_repitition(?):
+    user_choice = input("Do you want to practice spaced repitition?: Y/N")
+    if user_choice == "N":
+        print("Thanks for playing!")
+        return
+    elif user_choice == "Y":
+        for i in wrong_answer:
+            print(f"What does {key} mean?: ")
 
 
-main(loteria_dict)
-#Randomize big cards
-# def randomize_cards(dictionary_cards):
-#     keys = list(dictionary_cards.keys())
-#     random.shuffle(keys) 
-#     print(keys)
+if __name__== "__main__":
+    main(loteria_dict)
+
